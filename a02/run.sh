cb659871c938fecb2b0b16f270700d302951607f
David Halama <maria_dev@sandbox-hdp.hortonworks.com>
2020-09-19 02:44:13 +0000

    fix run

--- a02/run.sh
+++ a02/run.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+DATE=$(date +"%Y%m%d%H%M%S")
+
 hadoop_cmd="hadoop jar /usr/hdp/2.6.5.0-292/hadoop-mapreduce/hadoop-streaming.jar"
 hadoop_files="-files ./a2/mapper.py,./a2/reducer.py"
 hadoop_io="-input /user/maria_dev/hadoopTest/wcinput/* -output /user/maria_dev/hadoopTest/wcoutput"
 hadoop_map="-mapper mapper.py -reducer reducer.py"
 
-hdfs dfs mkdir -p /user/maria_dev/hadoopTest
-hdfs dfs mkdir -p /user/maria_dev/hadoopTest/wcinput
-hdfs dfs rm -r /user/maria_dev/hadoopTest/wcinput/*
+hdfs dfs -mkdir -p /user/maria_dev/hadoopTest
+hdfs dfs -mkdir -p /user/maria_dev/hadoopTest/wcinput
+hdfs dfs -rm -r /user/maria_dev/hadoopTest/wcinput/*
 hdfs dfs -copyFromLocal ./a2/input.txt /user/maria_dev/hadoopTest/wcinput/
-hdfs dfs rm /user/maria_dev/wcoutput
+#hdfs dfs -rmdir --ignore-fail-on-non-empty  /user/maria_dev/hadoopTest/wcoutput
+hdfs dfs -mv /user/maria_dev/hadoopTest/wcoutput /user/maria_dev/hadoopTest/wcouput_$DATE
 
 
 $hadoop_cmd $hadoop_files $hadoop_io $hadoop_map
