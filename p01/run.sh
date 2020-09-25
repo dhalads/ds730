0e5324cc492026250cac13a41158d3fa69749003
David Halama <maria_dev@sandbox-hdp.hortonworks.com>
2020-09-25 11:41:30 +0000

    changed files

--- p01/run.sh
+++ p01/run.sh
@@ -6,9 +6,9 @@ DATE=$(date +"%Y%m%d%H%M%S")
 testFolder="/user/maria_dev/hadoopTest_$label"
 
 hadoop_cmd="hadoop jar /usr/hdp/2.6.5.0-292/hadoop-mapreduce/hadoop-streaming.jar"
-hadoop_files="-files ./a2/mapper.py,./a2/reducer.py"
+hadoop_files="-files ./p1/mapper1.py,./p1/reducer1.py"
 hadoop_io="-input $testFolder/wcinput/* -output $testFolder/wcoutput"
-hadoop_map="-mapper mapper.py -reducer reducer.py"
+hadoop_map="-mapper mapper1.py -reducer reducer1.py"
 
 
 
@@ -17,4 +17,6 @@ echo $testFolder
 hdfs dfs -mv $testFolder/wcoutput $testFolder/wcouput_$DATE
 
 
-$hadoop_cmd $hadoop_files $hadoop_io $hadoop_map
+myCommand="$hadoop_cmd $hadoop_files $hadoop_io $hadoop_map"
+echo $myCommand
+$myCommand
