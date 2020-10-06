0b60a244a51d4f82701bc2ae6bd3c4e3407800d8
David Halama <halama1668@uwlax.edu>
2020-10-05 22:17:31 -0500

    created zip file and script

---
+++ makeZip.py
@@ -0,0 +1,19 @@
+import shutil
+import sys
+import argparse
+
+def process_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("archiveName", help="archiveName")
+    parser.add_argument("root_dir", help="root_dir")
+    parser.add_argument("base_dir", help="base_dir")
+    args = parser.parse_args()
+    print(type(args))
+    return(args)
+    # print args.square**2
+
+args = process_args()
+print(args)
+
+
+shutil.make_archive(args.archiveName, 'zip', root_dir=args.root_dir, base_dir=args.base_dir)
